e9e9852d3740e9706067c6bb14806eb63c0cc448
JoaquinCT <jose.cortes.tenorio@est.una.ac.cr>
2026-06-19 15:48:21 -0600

    Update EvidenceDto.kt

--- app/src/main/java/com/example/leveluplife/data/network/dto/EvidenceDto.kt
+++ app/src/main/java/com/example/leveluplife/data/network/dto/EvidenceDto.kt
@@ -1,13 +1,13 @@
-package com.example.leveluplife.data.network.dto
+﻿package com.example.leveluplife.data.network.dto
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class EvidenceDto(
-    @SerialName("ID_EVIDENCE") val id: Int,
-    @SerialName("ID_HABIT_TASK") val taskId: Int,
-    @SerialName("DSC_EVIDENCE_PATH_URL") val url: String,
-    @SerialName("DSC_HEALTH_DATA_JSON") val healthDataJson: String? = null,
-    @SerialName("FEC_UPLOADED") val uploadedAt: String,
+    @SerialName("id") val id: Int,
+    @SerialName("habitTaskId") val taskId: Int,
+    @SerialName("url") val url: String,
+    @SerialName("healthDataJson") val healthDataJson: String? = null,
+    @SerialName("uploadedAt") val uploadedAt: String,
 )
